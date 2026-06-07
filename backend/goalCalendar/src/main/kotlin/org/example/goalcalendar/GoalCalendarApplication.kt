31b3666120d4b3f1f2d5cf301b33dbd9c3773fb2
Kallik <h.jarqueg@gmail.com>
2026-06-07 15:05:55 +0200

    main: fix main

--- backend/goalCalendar/src/main/kotlin/org/example/goalcalendar/GoalCalendarApplication.kt
+++ backend/goalCalendar/src/main/kotlin/org/example/goalcalendar/GoalCalendarApplication.kt
@@ -1,9 +1,10 @@
 package org.example.goalcalendar
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [UserDetailsServiceAutoConfiguration::class])
 class GoalCalendarApplication
 
 fun main(args: Array<String>) {
